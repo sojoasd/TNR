ef489f9a4778a8ace2fea1410abcd390410c1a9c
zeal yen <zealyen@noodoe.com>
2020-02-29 14:58:06 +0800

    remove refresh api

--- src/restful-server/account.ts
+++ src/restful-server/account.ts
@@ -2,10 +2,8 @@ import express from "express";
 const router = express.Router();
 import ActionController from "./actionController";
 import UserService from "../service/userService";
-import { authDirective } from "./middleware/authDirective";
 
 router.get("/login", ActionController.callAsync(UserService.login));
 router.get("/afterLoginResponseToken", ActionController.callAsync(UserService.afterLoginResponseToken));
-router.post("/refreshToken", ActionController.callAsync(UserService.refreshToken));
 
 export default router;
